fac928acbf7226753c9b4305ed43ccd339b049c9
chrismccabe17 <115700756+chrismccabe17@users.noreply.github.com>
2023-02-28 10:54:15 +0000

    Update streamline_app.py
    changed header and added toast

--- streamline_app.py
+++ streamline_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast menu')
+streamlit.header('Breakfast favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard boiled, free-range Egg')
+streamlit.text('Avocado toast')
 streamlit.header('Build your own fruit smoothie')
