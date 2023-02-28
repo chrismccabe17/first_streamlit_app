c44d6a4ef030dcf7f598206f9b1190486d308c9c
chrismccabe17 <115700756+chrismccabe17@users.noreply.github.com>
2023-02-28 11:06:51 +0000

    Update streamline_app.py
    emoji

--- streamline_app.py
+++ streamline_app.py
@@ -4,7 +4,7 @@ streamlit.header('Breakfast favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard boiled, free-range Egg')
-streamlit.text(:avocado:,'Avocado toast') 
+streamlit.text(':avocado:','Avocado toast') 
 streamlit.header('Build your own fruit smoothie')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
