dccdc770d35f1c4ec15c570da396de876166bbac
chrismccabe17 <115700756+chrismccabe17@users.noreply.github.com>
2023-03-07 13:28:14 +0000

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -25,3 +25,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+
